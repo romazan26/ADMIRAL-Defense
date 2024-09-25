4a04f7be0550f2c0eaa6da9b02e2ae36cd1b7508
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-09-25 12:21:48 +0300

    Initial Commit

---
+++ ADMIRAL Defense/ADMIRAL_DefenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADMIRAL_DefenseApp.swift
+//  ADMIRAL Defense
+//
+//  Created by Роман on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ADMIRAL_DefenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
